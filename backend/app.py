b8cede095e47b021fad2107d6ba67b094c2d3132
Joelfernandes30 <139780764+Joelfernandes30@users.noreply.github.com>
2025-08-13 21:18:02 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,7 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
-from pymongo import MongoClient
-from datetime import datetime
-import os
-from dotenv import load_dotenv
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/)
+@app.route("/")
 def hello():
     return "hi"
-    
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
